b3a2c8eb28117cb469822e8b09343db711c1d6a4
kunaldhongade <kunaldhongade1@gmail.com>
2024-09-30 17:13:42 +0530

    Add: New Contract Deployed

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,5 +1,5 @@
 export const NETWORK = import.meta.env.VITE_APP_NETWORK ?? "testnet";
-export const MODULE_ADDRESS = "0x5fbab942388be12bc96e623fcc22d7c71bd76bede6a0b828de4c351e7aebcc1e";
+export const MODULE_ADDRESS = "0x25c8f2d9f9f8da2e858ce241b17fc32b9a157977dd1c8089b39115a5c459b4e7";
 export const CREATOR_ADDRESS = import.meta.env.VITE_COLLECTION_CREATOR_ADDRESS;
 export const COLLECTION_ADDRESS = import.meta.env.VITE_COLLECTION_ADDRESS;
 export const IS_DEV = Boolean(import.meta.env.DEV);
